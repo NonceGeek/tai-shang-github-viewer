bb95b2d2c4f6abc44cc979a185fdb4958a9d2ad8
qiwihui <qwh005007@gmail.com>
2022-03-13 14:53:52 +0000

    Deploying to gh-pages from @ WeLightProject/github-issue-viewer@584ad974e3694de4c6c46af34649d047872c9175 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bc437f4ff35c95d372573bc5822b5f31.js"
+  "/precache-manifest.2f3414912e549111cc893a1d274f3413.js"
 );
 
 workbox.clientsClaim();
